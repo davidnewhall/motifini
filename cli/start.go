960520507e0dac302d43037790d781d7a0e27233
davidnewhall2 <david@sleepers.pro>
2019-09-10 00:43:51 -0700

    move things around

--- cli/start.go
+++ cli/start.go
@@ -15,12 +15,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/spf13/pflag"
-
 	"github.com/BurntSushi/toml"
-	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
-
+	"github.com/spf13/pflag"
 	"golift.io/imessage"
 	"golift.io/securityspy"
 	"golift.io/subscribe"
@@ -175,8 +172,6 @@ func (m *Motifini) Run() error {
 	return m.StartServer()
 }
 
-
-
 func (m *Motifini) startiMessage() error {
 	var err error
 	m.Msgs, err = imessage.Init(&imessage.Config{
@@ -194,29 +189,6 @@ func (m *Motifini) startiMessage() error {
 	// Listen to all incoming imessages, pass them to our handler.
 	m.Msgs.IncomingCall(".*", m.recvMessageHandler)
 	return errors.Wrap(m.Msgs.Start(), "starting imessage")
-}
-
-// StartServer creates the http routers and starts http server
-func (m *Motifini) StartServer() error {
-	log.Printf("Listening on port %d", m.Config.Global.Port)
-	r := mux.NewRouter()
-	r.Handle("/debug/vars", http.DefaultServeMux).Methods("GET")
-	r.HandleFunc("/api/v1.0/send/imessage/video/{to}/{camera}", m.sendVideoHandler).Methods("GET")
-	r.HandleFunc("/api/v1.0/send/imessage/picture/{to}/{camera}", m.sendPictureHandler).Methods("GET")
-	r.HandleFunc("/api/v1.0/send/imessage/msg/{to}", m.sendMessageHandler).Methods("GET").Queries("msg", "{msg}")
-	r.HandleFunc("/api/v1.0/event/{cmd:remove|update|add|notify}/{event}", m.eventsHandler).Methods("POST")
-	// need to figure out what user interface will use these methods.
-	r.HandleFunc("/api/v1.0/sub/{cmd:subscribe|unsubscribe|pause|unpause}/{api}/{contact}/{event}", m.subsHandler).Methods("GET")
-	r.PathPrefix("/").HandlerFunc(m.handleAll)
-	http.Handle("/", r)
-	m.Server = &http.Server{
-		Addr:         fmt.Sprintf("127.0.0.1:%d", m.Config.Global.Port),
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      r, // *mux.Router
-	}
-	return m.Server.ListenAndServe()
 }
 
 // taskPoller runs things at an interval and
