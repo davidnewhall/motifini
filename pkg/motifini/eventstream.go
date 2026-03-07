661a23190ec8ee7f8d411c3e623a637df36610a9
David Newhall II <david@sleepers.pro>
2026-03-06 19:09:57 -0800

    fix height on captures

--- pkg/motifini/eventstream.go
+++ pkg/motifini/eventstream.go
@@ -101,7 +101,7 @@ func (m *Motifini) handleCameraMotion(e securityspy.Event) {
 	}
 
 	err := e.Camera.SaveVideo(
-		&securityspy.VidOps{ACodec: "ulaw"}, 5*time.Second, 999*999, path) // nolint:gomnd
+		&securityspy.VidOps{ACodec: "ulaw", Height: 600}, 5*time.Second, 1*1024*1024, path) // nolint:gomnd
 	if err != nil {
 		m.Error.Printf("[%v] event.Camera.SaveVideo: %v", id, err)
 		return
